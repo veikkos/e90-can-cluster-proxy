0832a612dc7b49468f3c1b5f8be973fd4858f25b
Veikko Soininen <veikko.soininen@gmail.com>
2025-06-10 19:53:45 +0300

    More data

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ const toTF = (bool) => (bool ? 'T' : 'F');
 const server = udp.createServer(function (buff) {
     const data = {
         car: buff.toString('ascii', 4, 8),
-        flags: buff.readUInt8(9),
+        flags: buff.readUInt16LE(8),
         gear: buff.readUInt8(10),
         plid: buff.readUInt8(11),
         speed: buff.readFloatLE(12),
@@ -58,15 +58,38 @@ const server = udp.createServer(function (buff) {
         clutch: buff.readFloatLE(56)
     };
 
+    console.log(data);
+
+    const formatTimestamp = () => {
+        const now = new Date();
+        const pad2 = (n) => n.toString().padStart(2, '0');
+
+        return (
+            now.getFullYear().toString() +
+            pad2(now.getMonth() + 1) +
+            pad2(now.getDate()) +
+            pad2(now.getHours()) +
+            pad2(now.getMinutes()) +
+            pad2(now.getSeconds())
+        );
+    };
+
     const asciiMsg =
+        formatTimestamp() +
         pad(data.rpm, 5) +
         pad(data.speed * 3.6 * 10, 4) +
         pad(data.gear, 1) +
         pad(data.engtemp, 3) +
         pad(data.fuel * 1000, 4) +
-        toTF(data.throttle > 0.5) +
-        toTF(data.brake > 0.5) +
-        toTF(data.clutch > 0.5);
+        toTF(data.showlights & (1 << 0)) +   // SHIFT
+        toTF(data.showlights & (1 << 1)) +   // FULLBEAM
+        toTF(data.showlights & (1 << 2)) +   // HANDBRAKE
+        toTF(data.showlights & (1 << 4)) +   // TC
+        toTF(data.showlights & (1 << 5)) +   // SIGNAL_L
+        toTF(data.showlights & (1 << 6)) +   // SIGNAL_R
+        toTF(data.showlights & (1 << 8)) +   // OILWARN
+        toTF(data.showlights & (1 << 9)) +   // BATTERY
+        toTF(data.showlights & (1 << 10));   // ABS
 
     if (serialPort.isOpen) {
         serialPort.write(asciiMsg + "\n");
